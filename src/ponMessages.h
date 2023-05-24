c553210fc8c17a036d175d7c75e75cb95a2dc5b8
Mohammad Saddam Mashuri <saddamonpc@gmail.com>
2023-05-24 20:10:45 +1000

    Add and update more motivational quotes

--- src/ponMessages.h
+++ src/ponMessages.h
@@ -2,6 +2,14 @@
 
 std::string messages[] = {
     "Do some stuff now so you can relax later on.",
-    "Cao ni ma",
-    "Wo fashe la divi"    
+    "\"Never put off till tomorrow what may be done day after tomorrow just as well.\" - Mark Twain",
+    "\"Wo fashe la divi\" - Some Chinese BF4 Soldier",
+    "\"Procrastination is the thief of time, collar him.\" - Charles Dickens",
+    "\"A year from now you may wish you had started today.\" - Karen Lamb"," 
+    "\"It is easier to resist at the beginning than at the end.\" - Leonardo da Vinci",
+    "\"A day can really slip by when you're deliberately avoiding what you're supposed to do.\" - Bill Watterson",
+    "\"If worrying about something doesn't lead to a solution, then its not worth worrying about in the first place.\" - Hachikuji Mayoi, Monogatari Series",
+    "\"People have to save themselves. One person saving another is impossible.\" - Meme Oshino, Monogatari Series",
+    "\"People can run from the things they don't like all they want. But if they're just averting their eyes, they're not running. As long as you think the current situation is okay, no one can help.\" - Araragi's Mother",
+    "\"Change before you have to.\" - Jack Welch"
 };
